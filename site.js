61415af903ca366f5f650a27efe2fbc18a406272
SethThomaspowered <athomas.seth@gmail.com>
2021-10-20 08:55:41 -0400

    added menu

---
+++ site.js
@@ -0,0 +1,46 @@
+const menu = [
+    {name: "Coffee",
+    description: "",
+    menuItems: [
+        {name: "Coffee of the Day",
+        price: 4.99,
+        description: "",
+        },
+        {name: "Cafe Au Lait",
+        price: 3.99,
+        description: "",
+        },
+    ],
+    },
+    {name: "Teas",
+    description: "",
+    menuItems: [
+        {
+            name: "Loose-Leaf Tea",
+            price: 4.99,
+            description: "",
+        },
+        {
+            name: "Tea Au Lait",
+            price: 3.99,
+            description: "",
+        },
+    ],
+    },
+    {name: "Pastries/Snacks",
+    description: "",
+    menuItems: [
+        {
+            name: "Donut",
+            price: 1.99,
+            description: "",
+        },
+        {
+            name: "Cookie - 3 Pack",
+            price: 3.99,
+            description: "",
+        },
+    ],
+
+    }, 
+]
